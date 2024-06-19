681432f2a951f310fa651ba6053fa6240a8903fb
Abimael Espinal <36963285+abiespinal04@users.noreply.github.com>
2024-06-19 15:45:51 -0400

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by abimael espinal on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
